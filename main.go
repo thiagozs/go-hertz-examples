3aa53213786cb68dfb59e90cde8e3ef2cab67464
Thiago Zilli <thiago.zilli@gmail.com>
2022-12-05 16:30:51 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+type Test struct {
+	A string
+	B string
+}
+
+func main() {
+	h := server.Default()
+	h.StaticFS("/", &app.FS{Root: "./", GenerateIndexPages: true})
+
+	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, utils.H{"ping": "pong"})
+	})
+
+	h.GET("/json", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, &Test{
+			A: "aaa",
+			B: "bbb",
+		})
+	})
+
+	h.GET("/redirect", func(c context.Context, ctx *app.RequestContext) {
+		ctx.Redirect(consts.StatusMovedPermanently, []byte("http://www.google.com/"))
+	})
+
+	v1 := h.Group("/v1")
+	{
+		v1.GET("/hello/:name", func(c context.Context, ctx *app.RequestContext) {
+			fmt.Fprintf(ctx, "Hi %s, this is the response from Hertz.\n", ctx.Param("name"))
+		})
+	}
+
+	h.Spin()
+}
